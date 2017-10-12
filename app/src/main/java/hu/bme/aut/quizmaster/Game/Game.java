f2e671a3d0b0d88370f6a4b6cf19c78a98b30566
Gergő <takeeex@windowslive.com>
2017-10-12 09:43:11 +0200

    Organised code from EndlessGameActivity into Game

--- app/src/main/java/hu/bme/aut/quizmaster/Game/Game.java
+++ app/src/main/java/hu/bme/aut/quizmaster/Game/Game.java
@@ -3,31 +3,122 @@ package hu.bme.aut.quizmaster.Game;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import hu.bme.aut.quizmaster.Database.Question;
+import hu.bme.aut.quizmaster.Database.Request;
+import hu.bme.aut.quizmaster.R;
 
 public class Game {
     private Activity activity;
     private Context context;
+    private Random random;
+    // controls
+    private TextView tvTopic;
+    private TextView tvQuestion;
+    private TextView tvTimer;
+    private TextView tvResults;
+
+    private Button btnAnswer1;
+    private Button btnAnswer2;
+    private Button btnAnswer3;
+    private Button btnAnswer4;
+
+    private List<Question> questionList;
+
     private List<Player> players;
     private QuizMaster quizMaster;
+    private int TimeForAnswerinMillis = 30000;
 
-    public Game(Context context, Activity activity){
-        this.context = context;
+    public Game(Activity activity, Context context) {
         this.activity = activity;
-
-        initGame();
+        this.context = context;
+        this.questionList = Request.getInstance(context).getQuestionList();
+        this.random = new Random();
+        initControls();
     }
 
-    public void initGame(){
-
+    private void initControls() {
+        this.tvTopic = (TextView) activity.findViewById(R.id.tvTopic);
+        this.tvQuestion = (TextView) activity.findViewById(R.id.tvQuestion);
+        this.tvTimer = (TextView) activity.findViewById(R.id.tvTimer);
+        this.tvResults = (TextView) activity.findViewById(R.id.tvResults);
+        this.btnAnswer1 = (Button) activity.findViewById(R.id.btnQpAnsw1);
+        this.btnAnswer2 = (Button) activity.findViewById(R.id.btnQpAnsw2);
+        this.btnAnswer3 = (Button) activity.findViewById(R.id.btnQpAnsw3);
+        this.btnAnswer4 = (Button) activity.findViewById(R.id.btnQpAnsw4);
     }
 
-    public void startGame(){
-        nextQuestion();
+    public void startEndlessMode() {
+        while (!questionList.isEmpty()) {
+            getRandomQuestion();
+            startTimerForQuestion();
+        }
     }
 
-    public void nextQuestion(){
+    private void startTimerForQuestion() {
+        new CountDownTimer(TimeForAnswerinMillis, 1000) {
+            public void onTick(long millisUntilFinished) {
+                tvTimer.setText("" + millisUntilFinished / 1000);
+            }
 
+            public void onFinish() {
+                tvTimer.setText("Done!");
+            }
+        }.start();
+    }
+
+    public void startMultiplayerMode() {
+        // TODO
+    }
+
+    private void getRandomQuestion() {
+        int currQuestionIndex = random.nextInt(questionList.size());
+        Question question = questionList.get(currQuestionIndex);
+
+        List<String> answersForQuestion = Request.getInstance(context).getAllAnswer(question);
+
+        for (Button button : Arrays.asList(btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4)) {
+            int index = random.nextInt(answersForQuestion.size());
+            button.setText(answersForQuestion.get(index));
+            answersForQuestion.remove(index);
+        }
+
+        questionList.remove(currQuestionIndex);
+
+        tvResults.setText("Good anwers: 4/7");
+        tvTopic.setText(question.getTopic().toString());
+        tvQuestion.setText(question.getQuestionStr());
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        this.players.remove(player);
+    }
+
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public QuizMaster getQuizMaster() {
+        return quizMaster;
+    }
+
+    public void setQuizMaster(QuizMaster quizMaster) {
+        this.quizMaster = quizMaster;
     }
 }
--- app/src/main/java/hu/bme/aut/quizmaster/UserInterface/EndlessGameActivity.java
+++ app/src/main/java/hu/bme/aut/quizmaster/UserInterface/EndlessGameActivity.java
@@ -21,53 +21,8 @@ public class EndlessGameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // TODO: this and the other 3 classes
         Game game = new Game(this, this);
-
-        TextView tvTopic = (TextView) findViewById(R.id.tvTopic);
-        TextView tvQuestion = (TextView) findViewById(R.id.tvQuestion);
-        TextView tvTimer = (TextView) findViewById(R.id.tvTimer);
-        TextView tvResults = (TextView) findViewById(R.id.tvResults);
-        Button btnAnswer1 = (Button) findViewById(R.id.btnQpAnsw1);
-        Button btnAnswer2 = (Button) findViewById(R.id.btnQpAnsw2);
-        Button btnAnswer3 = (Button) findViewById(R.id.btnQpAnsw3);
-        Button btnAnswer4 = (Button) findViewById(R.id.btnQpAnsw4);
-
-        List<Question> questionList = Request.getInstance(this).getQuestionList();
-        Random random = new Random();
-        int formerQuestion = random.nextInt(questionList.size());
-        Question question = questionList.get(formerQuestion);
-        List<String> allAnswer = Request.getInstance(this).getAllAnswer(question);
-        int index = random.nextInt(allAnswer.size());
-        btnAnswer1.setText(allAnswer.get(index));
-        allAnswer.remove(index);
-        index = random.nextInt(allAnswer.size());
-        btnAnswer2.setText(allAnswer.get(index));
-        allAnswer.remove(index);
-        index = random.nextInt(allAnswer.size());
-        btnAnswer3.setText(allAnswer.get(index));
-        allAnswer.remove(index);
-        index = random.nextInt(allAnswer.size());
-        btnAnswer4.setText(allAnswer.get(index));
-        allAnswer.remove(index);
-
-        questionList.remove(formerQuestion);
-
-        tvResults.setText("Good anwers: 4/7");
-        tvTopic.setText(question.getTopic().toString());
-        tvQuestion.setText(question.getQuestionStr());
-
-        int TimeForAnswerinMillis = 30000;
-
-        new CountDownTimer(TimeForAnswerinMillis, 1000) {
-            public void onTick(long millisUntilFinished) {
-                tvTimer.setText("" + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                tvTimer.setText("Done!");
-            }
-        }.start();
+        game.startEndlessMode();
 
     }
 }
